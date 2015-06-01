af777e0eb422607e328d7588a3fc73760abb9609
kylejan <765108938@qq.com>
2015-06-01 18:07:58 +0800

    change to mysql again

--- tier.py
+++ tier.py
@@ -16,7 +16,7 @@
 
 import bcrypt
 import concurrent.futures
-import motor
+import MySQLdb
 import markdown
 import os.path
 import re
@@ -35,10 +35,10 @@ from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
 
-# define("mysql_host", default="127.0.0.1:3306", help="database host")
-# define("mysql_database", default="TIER", help="database name")
-# define("mysql_user", default="tier", help="database user")
-# define("mysql_password", default="jian", help="database password")
+define("mysql_host", default="127.0.0.1:3306", help="database host")
+define("mysql_database", default="TIER", help="database name")
+define("mysql_user", default="tier", help="database user")
+define("mysql_password", default="jian", help="database password")
 
 
 # A thread pool to be used for password hashing with bcrypt.
@@ -69,40 +69,38 @@ class Application(tornado.web.Application):
             cookie_secret="62oETzKXQAGaYdkL5gEmGeJJFuYq7EQnp2XdTP1o/Vo=",
             login_url="/auth/login",
             debug=True,
-            db=motor.MotorClient("mongodb://localhost:27017").TIER,
         )
         super(Application, self).__init__(handlers, **settings)
         
         # Have one global connection to the DB across all handlers
-        # self.db = torndb.Connection(
-        #     host=options.mysql_host, database=options.mysql_database,
-        #     user=options.mysql_user, password=options.mysql_password)
+        self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
 
-        self.db = motor.MotorClient().tier
+        self.maybe_create_tables()
 
-
-    # def maybe_create_tables(self):
+    def maybe_create_tables(self):
         
-    #     self.db.execute("SET SESSION default_storage_engine = 'InnoDB'")
-    #     self.db.execute("SET SESSION time_zone = '+0:00'")
-    #     self.db.execute("ALTER DATABASE CHARACTER SET 'utf8'")
+        self.db.execute("SET SESSION default_storage_engine = 'InnoDB'")
+        self.db.execute("SET SESSION time_zone = '+0:00'")
+        self.db.execute("ALTER DATABASE CHARACTER SET 'utf8'")
 
-    #     create_user_sql = "CREATE TABLE IF NOT EXISTS user ( \
-    #         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
-    #         email VARCHAR(100) NOT NULL UNIQUE, \
-    #         name VARCHAR(100) NOT NULL, \
-    #         hashed_password VARCHAR(100) NOT NULL \
-    #         )"
+        create_user_sql = "CREATE TABLE IF NOT EXISTS user ( \
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
+            email VARCHAR(100) NOT NULL UNIQUE, \
+            name VARCHAR(100) NOT NULL, \
+            hashed_password VARCHAR(100) NOT NULL \
+            )"
 
-    #     create_group_sql = "CREATE TABLE IF NOT EXISTS team ( \
-    #         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
-    #         name VARCHAR(100) NOT NULL UNIQUE, \
-    #         leader_id INT NOT NULL REFERENCES user(id), \
-    #         introduction TEXT NOT NULL \
-    #         )"
+        create_group_sql = "CREATE TABLE IF NOT EXISTS team ( \
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
+            name VARCHAR(100) NOT NULL UNIQUE, \
+            leader_id INT NOT NULL REFERENCES user(id), \
+            introduction TEXT NOT NULL \
+            )"
 
-    #     self.db.execute(create_user_sql)
-    #     self.db.execute(create_group_sql)
+        self.db.execute(create_user_sql)
+        self.db.execute(create_group_sql)
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -113,7 +111,7 @@ class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
         user_id = self.get_secure_cookie("tier_user")
         if not user_id: return None
-        return self.db.get("SELECT * FROM user WHERE id = %s", int(user_id))
+        return self.db.get("SELECT * FROM user WHERE id = %s", str(user_id))
 
     def whether_author_exists(self, name):
         sql = "SELECT * FROM user WHERE name = '{}'".format(name)
@@ -154,7 +152,11 @@ class AuthRegisterHandler(BaseHandler):
             "VALUES (%s, %s, %s)",
             email, name,
             hashed_password)
-        
+
+
+    def reg_user_callback(self, name):
+        doc = yield self.db.user.find({'name': name})
+        print doc
         self.set_secure_cookie("tier_user", str(user_id))
         self.redirect("/")
 
